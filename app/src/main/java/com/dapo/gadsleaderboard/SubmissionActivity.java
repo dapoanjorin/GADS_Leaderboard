8be7a55f1c556163d24bc38ae2ad2f151e47d96f
adedapo <femidpo3@gmail.com>
2020-09-11 19:35:29 +0100

    created layouts for success and failure dialogs

--- app/src/main/java/com/dapo/gadsleaderboard/SubmissionActivity.java
+++ app/src/main/java/com/dapo/gadsleaderboard/SubmissionActivity.java
@@ -1,5 +1,11 @@
 package com.dapo.gadsleaderboard;
 
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.view.*;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +15,27 @@ public class SubmissionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submission);
+
+        // transparent status bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        Rect displayRectangle = new Rect();
+        Window window = getWindow();
+        window.getDecorView().getWindowVisibleDisplayFrame(displayRectangle);
+
+        ViewGroup viewGroup = findViewById(R.id.container);
+//        viewGroup.setAlpha(1.0f);
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.confirmation_dialog, viewGroup, false);
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.DialogAlert));
+        builder.setView(dialogView);
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        alertDialog.show();;
+//       alertDialog.getWindow().setLayout((int) (displayRectangle.width() * 0.8f), (int) (displayRectangle.height() * 1.0f));
+        alertDialog.getWindow().setLayout(displayRectangle.width(), displayRectangle.height());
     }
 }
